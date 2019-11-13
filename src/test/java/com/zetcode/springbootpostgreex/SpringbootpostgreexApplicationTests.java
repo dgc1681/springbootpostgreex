573a68ec903f093ba509bf25e63a43bb938a1d62
Diego Gomez <diego.gomez@me.com>
2019-11-13 15:20:41 -0500

    cambio de texto index

---
+++ src/test/java/com/zetcode/springbootpostgreex/SpringbootpostgreexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zetcode.springbootpostgreex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootpostgreexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
